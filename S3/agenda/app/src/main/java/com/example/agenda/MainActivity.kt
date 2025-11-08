1571952e001c0570325480107369d72d6fbe082f
WilsonSalinasFlores <63874104+WilsonSalinasFlores@users.noreply.github.com>
2025-11-08 14:05:44 -0500

    add desing

--- S3/agenda/app/src/main/java/com/example/agenda/MainActivity.kt
+++ S3/agenda/app/src/main/java/com/example/agenda/MainActivity.kt
@@ -1,16 +1,54 @@
 package com.example.agenda
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        var txtusu = findViewById<EditText>(R.id.txt_user)
+        var txtcla = findViewById<EditText>(R.id.txt_cla)
+        var btningresar= findViewById<Button>(R.id.btn_ingresar)
+        var btnregistrasre= findViewById<Button>(R.id.btn_registrasre)
+
+        btningresar.setOnClickListener {
+            val url = "http://10.0.2.2/WSAGENDA/datos/persona.php"
+            var datos= JSONObject()
+            datos.put("accion","login")
+            datos.put("usuario",txtusu.text.toString())
+            datos.put("clave",txtcla.text.toString())
+            val rq= Volley.newRequestQueue(this)
+            val json= JsonObjectRequest(Request.Method.POST,url,datos,{
+                s->
+                try {
+                    val obj=(s)
+                    if(obj.getBoolean("estado")){
+                        val codigo=obj.getString("codigo")
+                    }else{
+                        Toast.makeText(applicationContext,"Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show()
+                    }
+                }catch (e: JSONException) {
+                    Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT).show()
+                }
+            },{ volleyError ->Toast.makeText(applicationContext,volleyError.message, Toast.LENGTH_SHORT).show()})
+            rq.add(json)
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
